5976f342322cfd7b658b6a74191bc4c555b62906
Elizabeth <liz@sterahi.com>
2020-09-16 17:52:58 -0400

    anything missed

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,9 +1,17 @@
 module Types
   class QueryType < Types::BaseObject
     field :all_items, [Types::ItemType], null: false
+    field :get_page, [Types::ItemType], null: false do
+      description "get selected page"
+      argument :page, Integer, required: true
+    end
 
     def all_items
       Item.all.limit(30).where.not(url: nil)
+    end
+    def get_page(page:)
+      puts 'getpage'
+      Item.all.limit(30).offset(page * 30)
     end
   end
 end
