d05addb3d57bcda55a36d232fa61090d7c8b8c96
Elizabeth <liz@sterahi.com>
2020-09-16 13:13:07 -0400

    limit entry types returned v2

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,7 +3,7 @@ module Types
     field :all_items, [Types::ItemType], null: false
 
     def all_items
-      Item.all.limit(30).where('type = story')
+      Item.all.limit(30).where.not(url: nil)
     end
   end
 end
